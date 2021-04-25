2dbad8044b59a0d805713fa6f55560d2e7b06ab8
CORP\zhangtongxin <zhangtongxin@360.cn>
2021-04-25 14:53:53 +0800

    init

---
+++ ipvsadm-1.26/cmake-build-debug/CMakeFiles/ipvsadm_1_26.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/config_stream.c" "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/cmake-build-debug/CMakeFiles/ipvsadm_1_26.dir/config_stream.c.o"
+  "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/dynamic_array.c" "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/cmake-build-debug/CMakeFiles/ipvsadm_1_26.dir/dynamic_array.c.o"
+  "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/ipvsadm.c" "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/cmake-build-debug/CMakeFiles/ipvsadm_1_26.dir/ipvsadm.c.o"
+  "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/libipvs/ip_vs_nl_policy.c" "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/cmake-build-debug/CMakeFiles/ipvsadm_1_26.dir/libipvs/ip_vs_nl_policy.c.o"
+  "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/libipvs/libipvs.c" "/cygdrive/d/gitee/c_open_project/ipvsadm-1.26/cmake-build-debug/CMakeFiles/ipvsadm_1_26.dir/libipvs/libipvs.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../libipvs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ipvsadm-1.26/cmake-build-debug/CMakeFiles/ipvsadm_1_26.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ipvsadm_1_26.dir/config_stream.c.o"
+  "CMakeFiles/ipvsadm_1_26.dir/dynamic_array.c.o"
+  "CMakeFiles/ipvsadm_1_26.dir/ipvsadm.c.o"
+  "CMakeFiles/ipvsadm_1_26.dir/libipvs/ip_vs_nl_policy.c.o"
+  "CMakeFiles/ipvsadm_1_26.dir/libipvs/libipvs.c.o"
+  "ipvsadm_1_26.exe"
+  "ipvsadm_1_26.pdb"
+  "libipvsadm_1_26.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ipvsadm_1_26.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
